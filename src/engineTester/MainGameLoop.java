fce420ee89a028a7dd38b9300f5bd5cb2c735178
leo100bis <leo.brignone@gmail.com>
2021-04-14 18:34:24 +0200

    first commit

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,53 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+
+import entities.Camera;
+import entities.Entity;
+import entities.Light;
+import models.RawModel;
+import models.TexturedModel;
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.OBJLoader;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+
+public class MainGameLoop {
+	
+	public static void main(String[] args) {
+		
+		DisplayManager.createDisplay();
+		Loader loader = new Loader();
+		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
+		
+		RawModel model = OBJLoader.loadObjModel("stall", loader);
+		
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50), 0,0,0,1);
+		
+		Camera camera = new Camera();
+		
+		Light light = new Light(new Vector3f(0,0,-20), new Vector3f(1,1,1));
+
+		while(!Display.isCloseRequested()) {
+			entity.increaseRotation(0,1,0);
+			camera.move();
+			renderer.prepare();
+			shader.start();
+			shader.loadLight(light);
+			shader.loadViewMatrix(camera);
+			renderer.render(entity, shader);
+			shader.start();
+			DisplayManager.updateDisplay();
+		}
+		
+		shader.cleanUp();
+		loader.cleanUp();
+		DisplayManager.closeDisplay();
+	}
+}
